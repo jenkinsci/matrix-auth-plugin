a974a6d2b23216fb80eb0bb3880ca53aabac9a37
Daniel Beck <daniel-beck@users.noreply.github.com>
2017-09-19 17:34:20 +0200

    Fix another warning

--- src/main/java/hudson/security/ProjectMatrixAuthorizationStrategy.java
+++ src/main/java/hudson/security/ProjectMatrixAuthorizationStrategy.java
@@ -99,7 +99,7 @@ public class ProjectMatrixAuthorizationStrategy extends GlobalMatrixAuthorizatio
     public ACL getACL(@Nonnull AbstractItem item) {
         if (Jenkins.getInstance().getPlugin("cloudbees-folder") != null) { // optional dependency
             if (item instanceof AbstractFolder) {
-                com.cloudbees.hudson.plugins.folder.properties.AuthorizationMatrixProperty p = (com.cloudbees.hudson.plugins.folder.properties.AuthorizationMatrixProperty) ((AbstractFolder) item).getProperties().get(com.cloudbees.hudson.plugins.folder.properties.AuthorizationMatrixProperty.class);
+                com.cloudbees.hudson.plugins.folder.properties.AuthorizationMatrixProperty p = ((AbstractFolder<?>) item).getProperties().get(com.cloudbees.hudson.plugins.folder.properties.AuthorizationMatrixProperty.class);
                 if (p != null) {
                     return p.getInheritanceStrategy().getEffectiveACL(p.getACL(), item);
                 }
