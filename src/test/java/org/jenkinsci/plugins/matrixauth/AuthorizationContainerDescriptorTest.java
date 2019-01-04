538fca693ae248fb738615f64460755e8bcc1fc5
Francisco Javier Fernandez Gonzalez <fjfernandez@cloudbees.com>
2019-01-04 18:10:19 +0100

    [JENKINS-55397] New Permission in scope test

--- src/test/java/org/jenkinsci/plugins/matrixauth/AuthorizationContainerDescriptorTest.java
+++ src/test/java/org/jenkinsci/plugins/matrixauth/AuthorizationContainerDescriptorTest.java
@@ -3,17 +3,25 @@ package org.jenkinsci.plugins.matrixauth;
 import hudson.model.Item;
 import hudson.model.Run;
 import hudson.security.GlobalMatrixAuthorizationStrategy;
+import hudson.security.Permission;
+import hudson.security.PermissionScope;
 import hudson.util.VersionNumber;
 import jenkins.model.Jenkins;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.localizer.Localizable;
+import org.jvnet.localizer.ResourceBundleHolder;
+
+import java.util.Locale;
 
 public class AuthorizationContainerDescriptorTest {
 
     @Rule
     public JenkinsRule r = new JenkinsRule(); // Needed to check the jenkins version
+
+    private Permission FAKE = new Permission(Item.PERMISSIONS, "Fake", new TestLocalizable(), Item.BUILD, PermissionScope.ITEM);
 
     @Test
     public void testImpliedNotes() {
@@ -33,6 +41,35 @@ public class AuthorizationContainerDescriptorTest {
             String description = new GlobalMatrixAuthorizationStrategy.DescriptorImpl().getDescription(Item.CANCEL);
             Assert.assertFalse(description.contains(Messages.GlobalMatrixAuthorizationStrategy_PermissionNotImpliedBy()));
             Assert.assertEquals(Jenkins.getVersion().isOlderThan(new VersionNumber("2.120")), description.contains(Messages.GlobalMatrixAuthorizationStrategy_PermissionImpliedBy(Item.PERMISSIONS.title.toString(), Item.BUILD.name)));
+        }
+
+        {
+            String description = new GlobalMatrixAuthorizationStrategy.DescriptorImpl().getDescription(FAKE);
+            Assert.assertFalse(description.contains(Messages.GlobalMatrixAuthorizationStrategy_PermissionNotImpliedBy()));
+            Assert.assertTrue(description.contains(Messages.GlobalMatrixAuthorizationStrategy_PermissionImpliedBy(Item.PERMISSIONS.title.toString(), Item.BUILD.name)));
+        }
+    }
+
+    private static class TestLocalizable extends Localizable {
+        private static final String MSG = "This permission has been created for this test class";
+
+        public TestLocalizable() {
+            super(null, "key", new Object[0]);
+        }
+
+        @Override
+        public String getKey() {
+            return "key";
+        }
+
+        @Override
+        public String toString(Locale locale) {
+            return MSG;
+        }
+
+        @Override
+        public String toString() {
+            return MSG;
         }
     }
 }
