4297d52cdbc5bf9458bd9593d29bac7525740f58
Francisco Javier Fernandez Gonzalez <fjfernandez@cloudbees.com>
2019-01-04 09:51:10 +0100

    [JENKINS-55397] Updating AuthorizationContainerDescriptorTest for newer core versions

--- src/test/java/org/jenkinsci/plugins/matrixauth/AuthorizationContainerDescriptorTest.java
+++ src/test/java/org/jenkinsci/plugins/matrixauth/AuthorizationContainerDescriptorTest.java
@@ -3,11 +3,18 @@ package org.jenkinsci.plugins.matrixauth;
 import hudson.model.Item;
 import hudson.model.Run;
 import hudson.security.GlobalMatrixAuthorizationStrategy;
+import hudson.util.VersionNumber;
 import jenkins.model.Jenkins;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
 
 public class AuthorizationContainerDescriptorTest {
+
+    @Rule
+    public JenkinsRule r = new JenkinsRule(); // Needed to check the jenkins version
+
     @Test
     public void testImpliedNotes() {
         { // no message on Administer
@@ -22,10 +29,10 @@ public class AuthorizationContainerDescriptorTest {
             Assert.assertFalse(description.contains(Messages.GlobalMatrixAuthorizationStrategy_PermissionImpliedBy(Jenkins.PERMISSIONS.title.toString(), Jenkins.ADMINISTER.name)));
         }
 
-        { // Item.CANCEL is implied by Item.BUILD (at least up to core 2.111)
+        { // Item.CANCEL is implied by Item.BUILD until 2.119. From 2.120 is implied by Permission.UPDATE, so the description changes.
             String description = new GlobalMatrixAuthorizationStrategy.DescriptorImpl().getDescription(Item.CANCEL);
             Assert.assertFalse(description.contains(Messages.GlobalMatrixAuthorizationStrategy_PermissionNotImpliedBy()));
-            Assert.assertTrue(description.contains(Messages.GlobalMatrixAuthorizationStrategy_PermissionImpliedBy(Item.PERMISSIONS.title.toString(), Item.BUILD.name)));
+            Assert.assertEquals(Jenkins.getVersion().isOlderThan(new VersionNumber("2.120")), description.contains(Messages.GlobalMatrixAuthorizationStrategy_PermissionImpliedBy(Item.PERMISSIONS.title.toString(), Item.BUILD.name)));
         }
     }
 }
