1b57dbd908d9ff06827d06d80cbab1a9f0a11397
Daniel Beck <daniel-beck@users.noreply.github.com>
2019-05-02 18:29:25 +0200

    Add test

---
+++ src/test/java/org/jenkinsci/plugins/matrixauth/Jenkins57313Test.java
@@ -0,0 +1,29 @@
+package org.jenkinsci.plugins.matrixauth;
+
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import hudson.security.GlobalMatrixAuthorizationStrategy;
+import jenkins.model.Jenkins;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.Issue;
+import org.jvnet.hudson.test.JenkinsRule;
+
+public class Jenkins57313Test {
+    @Rule
+    public JenkinsRule j = new JenkinsRule();
+
+    @Test
+    @Issue("JENKINS-57313")
+    public void testFormValidation() throws Exception {
+        j.jenkins.setSecurityRealm(j.createDummySecurityRealm());
+        GlobalMatrixAuthorizationStrategy authorizationStrategy = new GlobalMatrixAuthorizationStrategy();
+        authorizationStrategy.add(Jenkins.ADMINISTER, "anonymous");
+        j.jenkins.setAuthorizationStrategy(authorizationStrategy);
+        HtmlPage page = j.createWebClient().goTo(authorizationStrategy.getDescriptor().getDescriptorUrl() + "/checkName?value=[alice]");
+        Assert.assertEquals(200, page.getWebResponse().getStatusCode());
+        String responseText = page.getWebResponse().getContentAsString();
+        Assert.assertTrue(responseText.contains("alice"));
+        Assert.assertTrue(responseText.contains("person.png"));
+    }
+}
