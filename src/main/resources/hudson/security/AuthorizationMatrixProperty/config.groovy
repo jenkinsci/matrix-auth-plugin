c29e1cd0a38f264527d84492f814a473492a736e
Tim Jacomb <timjacomb1+github@gmail.com>
2020-05-10 08:47:55 +0100

    Add blockWrapper to more locations

--- src/main/resources/hudson/security/AuthorizationMatrixProperty/config.groovy
+++ src/main/resources/hudson/security/AuthorizationMatrixProperty/config.groovy
@@ -8,9 +8,21 @@ def st = namespace("jelly:stapler")
 
 f.optionalBlock(name: 'useProjectSecurity', checked: instance != null, title: _("Enable project-based security")) {
     f.nested {
-        table(style: "width: 100%") {
+        blockWrapper {
             f.dropdownDescriptorSelector(title: _("Inheritance Strategy"), descriptors: InheritanceStrategyDescriptor.getApplicableDescriptors(my.class), field: 'inheritanceStrategy')
             st.include(class: "hudson.security.GlobalMatrixAuthorizationStrategy", page: "config")
+        }
+    }
+}
+
+def blockWrapper(Closure closure) {
+    if (context.getVariableWithDefaultValue("divBasedFormLayout", "false") == "true") {
+        div() {
+            closure.call()
+        }
+    } else {
+        table(style: "width: 100%") {
+            closure.call()
         }
     }
 }
