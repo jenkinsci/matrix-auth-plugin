b995ab1456360f60a95c22fa1aa59612d7d314b4
Tim Jacomb <timjacomb1+github@gmail.com>
2020-05-10 08:31:33 +0100

    JENKINS-56109 Compatibility with table => div

--- src/main/resources/org/jenkinsci/plugins/matrixauth/AuthorizationMatrixNodeProperty/config.groovy
+++ src/main/resources/org/jenkinsci/plugins/matrixauth/AuthorizationMatrixNodeProperty/config.groovy
@@ -7,8 +7,21 @@ def f = namespace(FormTagLib)
 def st = namespace("jelly:stapler")
 
 f.nested {
-    table(style: "width: 100%") {
+    blockWrapper {
         f.dropdownDescriptorSelector(title: _("Inheritance Strategy"), descriptors: InheritanceStrategyDescriptor.getApplicableDescriptors(my?.class?:hudson.model.Node.class), field: 'inheritanceStrategy')
         st.include(class: "hudson.security.GlobalMatrixAuthorizationStrategy", page: "config")
+    }
+    
+}
+
+def blockWrapper(Closure closure) {
+    if (context.getVariableWithDefaultValue("divBasedFormLayout", "false") == "true") {
+        div() {
+            closure.call()
+        }
+    } else {
+        table(style: "width: 100%") {
+            closure.call()
+        }
     }
 }
