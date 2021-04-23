45659adc6cd1ea64c48154d6d0d2998870523c6c
Daniel Beck <daniel-beck@users.noreply.github.com>
2021-04-23 13:44:52 +0200

    Update to 2.277.1, remove tables-to-div compatibility code

--- src/main/resources/org/jenkinsci/plugins/matrixauth/AuthorizationMatrixNodeProperty/config.groovy
+++ src/main/resources/org/jenkinsci/plugins/matrixauth/AuthorizationMatrixNodeProperty/config.groovy
@@ -4,11 +4,10 @@ import lib.FormTagLib
 import org.jenkinsci.plugins.matrixauth.inheritance.InheritanceStrategyDescriptor
 
 def f = namespace(FormTagLib)
-def c = namespace("/lib/matrixauth")
 def st = namespace("jelly:stapler")
 
 f.nested {
-    c.blockWrapper {
+    div {
         f.dropdownDescriptorSelector(title: _("Inheritance Strategy"), descriptors: InheritanceStrategyDescriptor.getApplicableDescriptors(my?.class?:hudson.model.Node.class), field: 'inheritanceStrategy')
         st.include(class: "hudson.security.GlobalMatrixAuthorizationStrategy", page: "config")
     }
