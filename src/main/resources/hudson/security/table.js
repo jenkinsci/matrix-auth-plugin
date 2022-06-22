047fce3a8f1ad36a44d00f4a90fb48714fbe7eff
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2022-06-22 07:08:16 +0100

    Fix tooltips with Tippy.js (#120)
    * Fix Tippy
    
    * Mostly working
    
    * Fix type display
    
    * Check tippy is present
    
    * Cleanup code
    
    * More compatibility
    
    * Consistency
    
    * Fix tooltips for permission descriptions
    
    * html-tooltip forward compat
    
    * Use forward compat from core
    
    Co-authored-by: Tim Jacomb <timjacomb1+github@gmail.com>
    Co-authored-by: Daniel Beck <daniel-beck@users.noreply.github.com>
    Co-authored-by: Daniel Beck <1831569+daniel-beck@users.noreply.github.com>

--- src/main/resources/hudson/security/table.js
+++ src/main/resources/hudson/security/table.js
@@ -40,8 +40,16 @@ Behaviour.specify(".matrix-auth-add-button", 'GlobalMatrixAuthorizationStrategy'
     findElementsBySelector(copy, ".stop img").each(function(item) {
       item.setAttribute("title", item.getAttribute("title").replace("__SID__", name).replace("__TYPE__", typeLabel));
     });
+
+    var tooltipAttributeName = getTooltipAttributeName();
+
     findElementsBySelector(copy, "input[type=checkbox]").each(function(item) {
-      item.setAttribute("title", item.getAttribute("title").replace("__SID__", name).replace("__TYPE__", typeLabel));
+      const tooltip = item.getAttribute(tooltipAttributeName);
+      if (tooltip) {
+        item.setAttribute(tooltipAttributeName, tooltip.replace("__SID__", name).replace("__TYPE__", typeLabel));
+      } else {
+        item.setAttribute("title", item.getAttribute("title").replace("__SID__", name).replace("__TYPE__", typeLabel));
+      }
     });
     table.appendChild(copy);
     Behaviour.applySubtree(findAncestor(table,"TABLE"),true);
@@ -191,6 +199,11 @@ Behaviour.specify(".global-matrix-authorization-strategy-table TD.stop A.migrate
   e = null; // avoid memory leak
 });
 
+function getTooltipAttributeName() {
+  var tippySupported = window.registerTooltips !== undefined;
+  return tippySupported ? 'html-tooltip' : 'tooltip';
+}
+
 /*
  * Whenever permission assignments change, this ensures that implied permissions get their checkboxes disabled.
  */
@@ -200,13 +213,16 @@ Behaviour.specify(".global-matrix-authorization-strategy-table td input", 'Globa
     // if this is a read-only UI (ExtendedRead / SystemRead), do not enable checkboxes
     return;
   }
+
+  var tooltipAttributeName = getTooltipAttributeName();
+
   var impliedByString = findAncestor(e, "TD").getAttribute('data-implied-by-list');
   var impliedByList = impliedByString.split(" ");
   var tr = findAncestor(e,"TR");
   e.disabled = false;
   let tooltip = YAHOO.lang.escapeHTML(findAncestor(e, "TD").getAttribute('data-tooltip-enabled'));
-  e.setAttribute('tooltip', tooltip); // before 2.335 -- TODO remove once baseline is new enough
-  e.nextSibling.setAttribute('tooltip', tooltip); // 2.335+
+  e.setAttribute(tooltipAttributeName, tooltip); // before 2.335 -- TODO remove once baseline is new enough
+  e.nextSibling.setAttribute(tooltipAttributeName, tooltip); // 2.335+
 
   for (var i = 0; i < impliedByList.length; i++) {
     var permissionId = impliedByList[i];
@@ -215,11 +231,16 @@ Behaviour.specify(".global-matrix-authorization-strategy-table td input", 'Globa
       if (reference.checked) {
         e.disabled = true;
         let tooltip = YAHOO.lang.escapeHTML(findAncestor(e, "TD").getAttribute('data-tooltip-disabled'));
-        e.setAttribute('tooltip', tooltip); // before 2.335 -- TODO remove once baseline is new enough
-        e.nextSibling.setAttribute('tooltip', tooltip); // 2.335+
+        e.setAttribute(tooltipAttributeName, tooltip); // before 2.335 -- TODO remove once baseline is new enough
+        e.nextSibling.setAttribute(tooltipAttributeName, tooltip); // 2.335+
       }
     }
   }
+
+  if (window.registerTooltips) {
+    window.registerTooltips(e.nextSibling.parentElement);
+  }
+
   e.onchange = function() {
     Behaviour.applySubtree(findAncestor(this,"TABLE"),true);
     return true;
