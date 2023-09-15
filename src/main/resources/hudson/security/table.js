16fb197cf5b9b879ede52b82e9e15355baa0a12b
Daniel Beck <daniel-beck@users.noreply.github.com>
2023-09-15 21:17:46 +0200

    More cleanup

--- src/main/resources/hudson/security/table.js
+++ src/main/resources/hudson/security/table.js
@@ -1,44 +1,49 @@
+/* global Behaviour, FormChecker, findElementsBySelector, findAncestor */
+
+function matrixAuthEscapeHtml(html) {
+  return html.replace(/'/g, "&apos;").replace(/"/g, "&quot;").replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+}
+
 /*
  * This handles the addition of new users/groups to the list.
  */
 Behaviour.specify(".matrix-auth-add-button", 'GlobalMatrixAuthorizationStrategy', 0, function(e) {
   e.onclick = function (e) {
-    var dataReference = e.target;
-    var master = document.getElementById(dataReference.getAttribute('data-table-id'));
-    var table = master.parentNode;
-    var type = dataReference.getAttribute('data-type');
-    var typeLabel = dataReference.getAttribute('data-type-label');
+    const dataReference = e.target;
+    const master = document.getElementById(dataReference.getAttribute('data-table-id'));
+    const table = master.parentNode;
+    const type = dataReference.getAttribute('data-type');
+    const typeLabel = dataReference.getAttribute('data-type-label');
 
-    var name = prompt(dataReference.getAttribute('data-message-prompt'));
+    const name = prompt(dataReference.getAttribute('data-message-prompt'));
     if (name == null) {
       return;
     }
-    if(name=="") {
+    if (name === "") {
       alert(dataReference.getAttribute('data-message-empty'));
       return;
     }
-    if(findElementsBySelector(table,"TR").find(function(n){return n.getAttribute("name")=='['+type+':'+name+']';})!=null) {
+    if (findElementsBySelector(table,"TR").find(function(n){
+      return n.getAttribute("name") === '[' + type + ':' + name + ']';
+    }) !== null) {
       alert(dataReference.getAttribute('data-message-error'));
       return;
     }
 
-    if(document.importNode!=null)
-      copy = document.importNode(master,true);
-    else
-      copy = master.cloneNode(true); // for IE
+    const copy = document.importNode(master,true);
     copy.removeAttribute("id");
     copy.removeAttribute("style");
-    copy.firstChild.innerHTML = YAHOO.lang.escapeHTML(name); // TODO consider setting innerText
-    copy.setAttribute("name",'['+type+':'+name+']');
+    copy.firstChild.innerHTML = matrixAuthEscapeHtml(name); // TODO consider setting innerText
+    copy.setAttribute("name",'[' + type + ':' + name + ']');
 
-    for(var child = copy.firstChild; child !== null; child = child.nextSibling) {
+    for (let child = copy.firstChild; child !== null; child = child.nextSibling) {
       if (child.hasAttribute('data-permission-id')) {
         child.setAttribute("data-tooltip-enabled", child.getAttribute("data-tooltip-enabled").replace("__SID__", name).replace("__TYPE__", typeLabel));
         child.setAttribute("data-tooltip-disabled", child.getAttribute("data-tooltip-disabled").replace("__SID__", name).replace("__TYPE__", typeLabel));
       }
     }
 
-    var tooltipAttributeName = 'data-html-tooltip';
+    const tooltipAttributeName = 'data-html-tooltip';
 
     findElementsBySelector(copy, ".stop a").forEach(function(item) {
       let oldTitle = item.getAttribute("title");
@@ -68,23 +73,23 @@ Behaviour.specify(".global-matrix-authorization-strategy-table TD.stop A.remove"
   e.onclick = function() {
     // Run ambiguity warning removal code: If all ambiguous rows are deleted, the warning needs to go as well
     // Order of operations: Find table ancestor, remove row, iterate over leftover rows
-    var table = findAncestor(this,"TABLE");
+    const table = findAncestor(this,"TABLE");
 
-    var tr = findAncestor(this,"TR");
+    const tr = findAncestor(this,"TR");
     tr.parentNode.removeChild(tr);
 
-    var tableRows = table.getElementsByTagName('tr');
+    const tableRows = table.getElementsByTagName('tr');
 
-    var hasAmbiguousRows = false;
+    let hasAmbiguousRows = false;
 
-    for (var i = 0; i < tableRows.length; i++) {
+    for (let i = 0; i < tableRows.length; i++) {
       if (tableRows[i].getAttribute('name') !== null && tableRows[i].getAttribute('name').startsWith('[EITHER')) {
         hasAmbiguousRows = true;
       }
     }
     if (!hasAmbiguousRows) {
-      var alertElements = document.getElementsByClassName("alert");
-      for (var i = 0; i < alertElements.length; i++) {
+      const alertElements = document.getElementsByClassName("alert");
+      for (let i = 0; i < alertElements.length; i++) {
         if (alertElements[i].hasAttribute('data-table-id') && alertElements[i].getAttribute('data-table-id') === table.getAttribute('data-table-id')) {
           alertElements[i].style.display = 'none'; // TODO animate this?
         }
@@ -93,7 +98,6 @@ Behaviour.specify(".global-matrix-authorization-strategy-table TD.stop A.remove"
 
     return false;
   }
-  e = null; // avoid memory leak
 });
 
 /*
@@ -109,7 +113,6 @@ Behaviour.specify(".global-matrix-authorization-strategy-table TD.stop A.selecta
     Behaviour.applySubtree(findAncestor(this,"TABLE"),true);
     return false;
   };
-  e = null; // avoid memory leak
 });
 
 /*
@@ -125,7 +128,6 @@ Behaviour.specify(".global-matrix-authorization-strategy-table TD.stop A.unselec
     Behaviour.applySubtree(findAncestor(this,"TABLE"),true);
     return false;
   };
-  e = null; // avoid memory leak
 });
 
 /*
@@ -133,18 +135,18 @@ Behaviour.specify(".global-matrix-authorization-strategy-table TD.stop A.unselec
  */
 Behaviour.specify(".global-matrix-authorization-strategy-table TD.stop A.migrate", 'GlobalMatrixAuthorizationStrategy', 0, function(e) {
   e.onclick = function() {
-    var tr = findAncestor(this,"TR");
-    var name = tr.getAttribute('name');
+    const tr = findAncestor(this,"TR");
+    const name = tr.getAttribute('name');
 
-    var newName = name.replace('[EITHER:', '[USER:'); // migrate_user behavior
+    let newName = name.replace('[EITHER:', '[USER:'); // migrate_user behavior
     if (this.classList.contains('migrate_group')) {
       newName = name.replace('[EITHER:', '[GROUP:');
     }
 
-    var table = findAncestor(this,"TABLE");
-    var tableRows = table.getElementsByTagName('tr');
-    var newNameElement = null;
-    for (var i = 0; i < tableRows.length; i++) {
+    const table = findAncestor(this,"TABLE");
+    const tableRows = table.getElementsByTagName('tr');
+    let newNameElement = null;
+    for (let i = 0; i < tableRows.length; i++) {
       if (tableRows[i].getAttribute('name') === newName) {
         newNameElement = tableRows[i];
         break;
@@ -160,19 +162,19 @@ Behaviour.specify(".global-matrix-authorization-strategy-table TD.stop A.migrate
       tr.removeAttribute('data-checked');
 
       // remove migration buttons from updated row
-      var buttonContainer = findAncestor(this, "DIV");
-      var migrateButtons = buttonContainer.getElementsByClassName('migrate');
-      for (var i = migrateButtons.length - 1; i >= 0; i--) {
+      const buttonContainer = findAncestor(this, "DIV");
+      const migrateButtons = buttonContainer.getElementsByClassName('migrate');
+      for (let i = migrateButtons.length - 1; i >= 0; i--) {
         buttonContainer.removeChild(migrateButtons[i]);
       }
     } else {
       // there's already a row for the migrated name (unusual but OK), so merge them
 
       // migrate permissions from this row
-      var ambiguousPermissionInputs = tr.getElementsByTagName("INPUT");
-      var unambiguousPermissionInputs = newNameElement.getElementsByTagName("INPUT");
-      for (var i = 0; i < ambiguousPermissionInputs.length; i++){
-        if(ambiguousPermissionInputs[i].type == "checkbox") {
+      const ambiguousPermissionInputs = tr.getElementsByTagName("INPUT");
+      const unambiguousPermissionInputs = newNameElement.getElementsByTagName("INPUT");
+      for (let i = 0; i < ambiguousPermissionInputs.length; i++){
+        if (ambiguousPermissionInputs[i].type === "checkbox") {
           unambiguousPermissionInputs[i].checked |= ambiguousPermissionInputs[i].checked;
         }
         newNameElement.classList.add('highlight-entry');
@@ -183,16 +185,16 @@ Behaviour.specify(".global-matrix-authorization-strategy-table TD.stop A.migrate
     }
     Behaviour.applySubtree(table, true);
 
-    var hasAmbiguousRows = false;
+    let hasAmbiguousRows = false;
 
-    for (var i = 0; i < tableRows.length; i++) {
+    for (let i = 0; i < tableRows.length; i++) {
       if (tableRows[i].getAttribute('name') !== null && tableRows[i].getAttribute('name').startsWith('[EITHER')) {
         hasAmbiguousRows = true;
       }
     }
     if (!hasAmbiguousRows) {
-      var alertElements = document.getElementsByClassName("alert");
-      for (var i = 0; i < alertElements.length; i++) {
+      let alertElements = document.getElementsByClassName("alert");
+      for (let i = 0; i < alertElements.length; i++) {
         if (alertElements[i].hasAttribute('data-table-id') && alertElements[i].getAttribute('data-table-id') === table.getAttribute('data-table-id')) {
           alertElements[i].style.display = 'none'; // TODO animate this?
         }
@@ -201,35 +203,34 @@ Behaviour.specify(".global-matrix-authorization-strategy-table TD.stop A.migrate
 
     return false;
   };
-  e = null; // avoid memory leak
 });
 
 /*
  * Whenever permission assignments change, this ensures that implied permissions get their checkboxes disabled.
  */
 Behaviour.specify(".global-matrix-authorization-strategy-table td input", 'GlobalMatrixAuthorizationStrategy', 0, function(e) {
-  var table = findAncestor(e, "TABLE");
+  const table = findAncestor(e, "TABLE");
   if (table.classList.contains('read-only')) {
     // if this is a read-only UI (ExtendedRead / SystemRead), do not enable checkboxes
     return;
   }
 
-  var tooltipAttributeName = 'data-html-tooltip';
+  const tooltipAttributeName = 'data-html-tooltip';
 
-  var impliedByString = findAncestor(e, "TD").getAttribute('data-implied-by-list');
-  var impliedByList = impliedByString.split(" ");
-  var tr = findAncestor(e,"TR");
+  const impliedByString = findAncestor(e, "TD").getAttribute('data-implied-by-list');
+  const impliedByList = impliedByString.split(" ");
+  const tr = findAncestor(e, "TR");
   e.disabled = false;
-  let tooltip = YAHOO.lang.escapeHTML(findAncestor(e, "TD").getAttribute('data-tooltip-enabled'));
+  let tooltip = matrixAuthEscapeHtml(findAncestor(e, "TD").getAttribute('data-tooltip-enabled'));
   e.nextSibling.setAttribute(tooltipAttributeName, tooltip);
 
-  for (var i = 0; i < impliedByList.length; i++) {
-    var permissionId = impliedByList[i];
-    var reference = tr.querySelector("td[data-permission-id='" + permissionId + "'] input");
+  for (let i = 0; i < impliedByList.length; i++) {
+    let permissionId = impliedByList[i];
+    let reference = tr.querySelector("td[data-permission-id='" + permissionId + "'] input");
     if (reference !== null) {
       if (reference.checked) {
         e.disabled = true;
-        let tooltip = YAHOO.lang.escapeHTML(findAncestor(e, "TD").getAttribute('data-tooltip-disabled'));
+        let tooltip = matrixAuthEscapeHtml(findAncestor(e, "TD").getAttribute('data-tooltip-disabled'));
         e.nextSibling.setAttribute(tooltipAttributeName, tooltip);
       }
     }
@@ -240,10 +241,9 @@ Behaviour.specify(".global-matrix-authorization-strategy-table td input", 'Globa
   }
 
   e.onchange = function() {
-    Behaviour.applySubtree(findAncestor(this,"TABLE"),true);
+    Behaviour.applySubtree(findAncestor(this,"TABLE"), true);
     return true;
   };
-  e = null; // avoid memory leak
 });
 
 /*
