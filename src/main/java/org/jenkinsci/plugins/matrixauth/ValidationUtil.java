42f2d3442cf414b9085f8009197d55a2c8d3d1e0
Tim Jacomb <timjacomb1@gmail.com>
2026-05-13 22:36:44 +0100

    Address review comments

--- src/main/java/org/jenkinsci/plugins/matrixauth/ValidationUtil.java
+++ src/main/java/org/jenkinsci/plugins/matrixauth/ValidationUtil.java
@@ -220,7 +220,7 @@ class ValidationUtil {
             // fall through next
         } catch (AuthenticationException e) {
             // other seemingly unexpected error.
-            return FormValidation.error(e, "Failed to test the validity of the user ID " + userName);
+            return FormValidation.error(e, "Failed to test the validity of the username " + userName);
         }
         return null;
     }
